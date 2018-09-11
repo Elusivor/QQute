3f3830a5c94dc12ad00812d747872ef69e276116
James Thompson <cthompson88@ymail.com>
2018-09-11 14:08:10 +1200

    Cache action filter attribute added

---
+++ QQute/Filters/CacheAF.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace QQute.Filters
+{
+    public class CacheAF : ActionFilterAttribute
+    {
+        public override Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            return base.OnActionExecutingAsync(actionContext, cancellationToken);
+        }
+
+
+
+        public override Task OnActionExecutedAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
+        {
+            actionExecutedContext.Response.Headers.CacheControl = new CacheControlHeaderValue()
+            {
+                Private = true,
+                Public = false,
+                NoStore = false,
+                MaxAge = TimeSpan.FromSeconds(10)
+            };
+
+            //Pragma
+            actionExecutedContext.Response.Headers.Date = DateTime.UtcNow;
+            actionExecutedContext.Response.Content.Headers.Expires = DateTime.UtcNow.AddSeconds(10);
+
+
+
+
+            return base.OnActionExecutedAsync(actionExecutedContext, cancellationToken);
+        }
+
+
+    }
+}
