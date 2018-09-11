17839c654d388ef56946a036499ce2cc2c9f1030
James Thompson <cthompson88@ymail.com>
2018-09-11 14:29:35 +1200

    Updated cacheAF and added Games controller

--- QQute/Filters/CacheAF.cs
+++ QQute/Filters/CacheAF.cs
@@ -9,6 +9,12 @@ namespace QQute.Filters
 {
     public class CacheAF : ActionFilterAttribute
     {
+        private bool _allowCache;
+
+        public CacheAF(bool DoYouWantCache = false)
+        {
+            _allowCache = DoYouWantCache;
+        }
         public override Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
         {
             return base.OnActionExecutingAsync(actionContext, cancellationToken);
@@ -18,20 +24,19 @@ namespace QQute.Filters
 
         public override Task OnActionExecutedAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
         {
-            actionExecutedContext.Response.Headers.CacheControl = new CacheControlHeaderValue()
+            if (_allowCache)
             {
-                Private = true,
-                Public = false,
-                NoStore = false,
-                MaxAge = TimeSpan.FromSeconds(10)
-            };
+                actionExecutedContext.Response.Headers.CacheControl = new CacheControlHeaderValue()
+                {
+                    Private = true,
+                    Public = false,
+                    NoStore = false,
+                    MaxAge = TimeSpan.FromSeconds(10)
+                };
 
-            //Pragma
-            actionExecutedContext.Response.Headers.Date = DateTime.UtcNow;
-            actionExecutedContext.Response.Content.Headers.Expires = DateTime.UtcNow.AddSeconds(10);
-
-
-
+                actionExecutedContext.Response.Headers.Date = DateTime.UtcNow;
+                actionExecutedContext.Response.Content.Headers.Expires = DateTime.UtcNow.AddSeconds(10);
+            }
 
             return base.OnActionExecutedAsync(actionExecutedContext, cancellationToken);
         }
