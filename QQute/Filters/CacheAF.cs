029e70be63f050c21c41ec3263abb5fb9ff14dd2
James Thompson <cthompson88@ymail.com>
2018-09-11 15:50:15 +1200

    Stopwatch added

--- QQute/Filters/CacheAF.cs
+++ QQute/Filters/CacheAF.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ using System.Web.Http.Filters;
 
 namespace QQute.Filters
 {
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     public class CacheAF : ActionFilterAttribute
     {
         private bool _allowCache;
@@ -15,12 +18,11 @@ namespace QQute.Filters
         {
             _allowCache = DoYouWantCache;
         }
+
         public override Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
         {
             return base.OnActionExecutingAsync(actionContext, cancellationToken);
         }
-
-
 
         public override Task OnActionExecutedAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
         {
@@ -40,7 +42,34 @@ namespace QQute.Filters
 
             return base.OnActionExecutedAsync(actionExecutedContext, cancellationToken);
         }
+    }
 
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class TimerAF : ActionFilterAttribute
+    {
+        const string _header = "QQ_API_Timer";
+        private string _property;
+        private string _timerName;
 
+        public TimerAF(string timerName = "")
+        {
+            _timerName = timerName;
+        }
+
+        public override Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            _property = $"{_timerName}_{actionContext.ActionDescriptor.ActionName}";
+            actionContext.Request.Properties.Add(_property, Stopwatch.StartNew());
+
+            return base.OnActionExecutingAsync(actionContext, cancellationToken);
+        }
+
+        public override Task OnActionExecutedAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
+        {
+            var timer = actionExecutedContext.Request.Properties[_property];
+            actionExecutedContext.Response.Headers.Add(_header, $"{((Stopwatch)timer).ElapsedMilliseconds.ToString()} ms");
+
+            return base.OnActionExecutedAsync(actionExecutedContext, cancellationToken);
+        }
     }
 }
